471a5ee3f3ac16d3f865e6f74b188d19900b5e7f
Derric samson <140136414+Derric01@users.noreply.github.com>
2025-03-31 17:16:44 +0530

    Add files via upload

---
+++ Math.js
@@ -0,0 +1,20 @@
+console.log(Math.PI);
+let x = 998.72355234;
+ let y = Math.round(x);
+ let y = Math.floor(x);
+ let y = Math.ceil(x);
+let y = Math.trunc(x)
+let g = 34;
+let h = -454
+
+ console.log(Math.abs(h));
+ console.log(Math.sqrt(x))
+ console.log(Math.tan(x));
+ console.log(Math.cos(x));
+ console.log(Math.sin(x));
+
+ console.log(Math.max(x,g,h));
+ console.log(Math.min(x,g,h));
+
+ console.log(Math.sign(x));
+ console.log(Math.pow(x,g));
