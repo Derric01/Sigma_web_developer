69f28ce8b296ce31acfde36a1f0524c860268f53
Derric samson <140136414+Derric01@users.noreply.github.com>
2025-04-24 21:22:33 +0530

    Add files via upload

---
+++ card-component/Card.jsx
@@ -0,0 +1,12 @@
+function Card() {
+  return (
+    <div className="card">
+      <img className="card-img" src="/ryuken.jpg" alt="profile" width="100" />
+
+      <h2 className="card-title">Ryuken Ishida</h2>
+      <p className="card-para">Once distant and cold, Ryuken Ishida walks a path of quiet redemption—fighting not for pride, but for the future he once abandoned.</p>
+    </div>
+  );
+}
+
+export default Card;
