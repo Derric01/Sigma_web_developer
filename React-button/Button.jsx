b6b8cdcc9f758b2770248da71c02fdc84285460f
Derric samson <140136414+Derric01@users.noreply.github.com>
2025-04-25 23:10:22 +0530

    Add files via upload

--- React-button/Button.jsx
+++ React-button/Button.jsx
@@ -2,9 +2,19 @@
 
 import styles from './Button.module.css'
 function  Button(){
+
+    const styles = {
+        backgroundColor:"antiquewhite",
+        fontSize: "20px",
+        color:"blue",
+        padding:" 10px 20px",
+        border: "none",
+        borderRadius:"5px",
+        cursor:"pointer"
+    }
     return(
         <>
-        <button className={styles.button}>Click me</button>
+        <button style={styles}>Click me</button>
 
         </>
     )
